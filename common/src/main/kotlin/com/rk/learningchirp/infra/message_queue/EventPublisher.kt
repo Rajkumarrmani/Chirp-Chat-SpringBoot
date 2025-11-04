ca4a67d0735afff452869f7dd42f652e2f4e6ddf
RaJManii <rajkumar.m@vivyacorp.com>
2025-11-04 16:53:55 +0530

    Publish fun

---
+++ common/src/main/kotlin/com/rk/learningchirp/infra/message_queue/EventPublisher.kt
@@ -0,0 +1,29 @@
+package com.rk.learningchirp.infra.message_queue
+
+import com.rk.learningchirp.domain.events.ChirpEvent
+import org.slf4j.LoggerFactory
+import org.springframework.amqp.rabbit.core.RabbitTemplate
+import org.springframework.stereotype.Component
+
+@Component
+class EventPublisher(
+    private val rabbitTemplate: RabbitTemplate
+) {
+    private val logger = LoggerFactory.getLogger(javaClass)
+
+    fun <T : ChirpEvent> publish(event: T) {
+        try {
+
+            rabbitTemplate.convertAndSend(
+                event.exchange,
+                event.eventKey,
+                event
+            )
+
+            logger.info("Successfully published event: $event")
+
+        } catch (e: Exception) {
+            logger.error("Failed to publish ${event.eventKey} event", e)
+        }
+    }
+}
---
+++ common/src/main/kotlin/com/rk/learningchirp/infra/message_queue/MessageQueues.kt
@@ -0,0 +1,5 @@
+package com.rk.learningchirp.infra.message_queue
+
+object MessageQueues {
+    const val NOTIFICATION_USER_EVENTS = "notification_user_events"
+}
